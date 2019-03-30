11b991e4078d4259e62d41aea4948ab606e80ca1
Jeff Jessie <j3ffjessie@icloud.com>
2019-03-30 11:16:28 -0400

    Initial Commit of DiceGame

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+// document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+function gamePlay(){
+  var player1 = document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  var player2 = document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+}
+
+function checkWinner(){
+  if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!!";
+  }
+  else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!!!";
+  }
+  else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML="Draw Game";
+  }
+}
+
+
+gamePlay();
+checkWinner();
